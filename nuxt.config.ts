62cc4561ff9df6ef39d4f54b7e8133b65b7c5a71
warmergroup <ulugbekjonsultonov4@gmail.com>
2025-04-23 03:23:46 +0500

    second commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@pinia/nuxt',
+  ],
 })
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <header class="header">Games List <span>found:4</span> </header>
+    <main>
+      <ul class="games-list">
+        <!-- Elementlar bu yerda joylashtiriladi -->
+      </ul>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  padding: 20px;
+}
+.games-list {
+  list-style: none;
+  padding: 0;
+}
+.header{
+  font-family: ;
+  font-size: 28px;
+  line-height: 36px;
+  vertical-align: middle;
+
+
+}
+</style>
